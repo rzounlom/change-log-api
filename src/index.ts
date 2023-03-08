8fb529c8d8150ebf351dd533c479908604ecbc81
Romaric Zounlome <rzounlom@gmail.com>
2023-03-07 20:21:08 -0500

    add build scripts; update index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as dotenv from "dotenv";
 
 import app from "./server.ts";
+import config from "./config";
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 // creates and starts a server for our API on a defined port
-app.listen(port, () => {
+app.listen(config.port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
